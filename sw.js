3a3af1a7a7e9f2ca1c1bb14717229dce9d4b76c9
norevery <norevery@users.noreply.github.com>
2025-06-11 20:29:28 +0000

    deploy: 49f01ea430bae791aa1248f0d448085824ff0a8c

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const version = '20250611185257';
+const version = '20250611202926';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/2021/06/13/%D0%B3%D0%B5%D0%BE%D0%BC%D0%B5%D1%82%D1%80%D1%96%D1%8F-%D0%B1%D0%B4%D0%B6%D0%BE%D0%BB%D0%B8%D0%BD%D0%B8%D1%85-%D1%81%D0%BE%D1%82/","/2021/05/24/%D0%B7%D0%BE%D0%BB%D0%BE%D1%82%D1%96-%D1%80%D0%B8%D0%B1%D0%BA%D0%B8/","/2021/05/17/%D0%BF%D1%80%D0%BE-%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D1%83/","/2021/05/03/%D0%BC%D0%B0%D0%BC%D0%BE/","/texts/","/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","/assets/logo.jpeg", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  return ["/2021/07/27/%D0%BF%D1%80%D0%BE-%D0%B7%D0%BE%D0%BE%D0%BF%D0%B0%D1%80%D0%BA/","/2021/07/07/%D0%BC%D1%96%D1%81%D1%82%D0%BE-%D1%81%D0%BE%D0%BD%D1%86%D1%8F/","/2021/06/24/goldilocks-conditions/","/2021/06/21/%D0%B5%D1%80%D0%B0%D1%82%D0%BE%D1%81%D1%84%D0%B5%D0%BD/","/2021/06/13/%D0%B3%D0%B5%D0%BE%D0%BC%D0%B5%D1%82%D1%80%D1%96%D1%8F-%D0%B1%D0%B4%D0%B6%D0%BE%D0%BB%D0%B8%D0%BD%D0%B8%D1%85-%D1%81%D0%BE%D1%82/","/2021/05/24/%D0%B7%D0%BE%D0%BB%D0%BE%D1%82%D1%96-%D1%80%D0%B8%D0%B1%D0%BA%D0%B8/","/2021/05/17/%D0%BF%D1%80%D0%BE-%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D1%83/","/2021/05/03/%D0%BC%D0%B0%D0%BC%D0%BE/","/texts/","/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/texts/page2/","/feed.xml","/assets/logo.jpeg", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
   ]
 }
 
